d46f0c22d0eb0f40c0a709526a7418316f659fe7
Bruno Sampaio Camba <brunocamba@gmail.com>
2024-02-01 11:39:22 -0300

    Update Program.cs

--- LegacyApp.Consumer/Program.cs
+++ LegacyApp.Consumer/Program.cs
@@ -16,8 +16,8 @@ namespace LegacyApp.Consumer
         	*/
 
             var userService = new UserService();
-            var addResult = userService.AddUser("Nick", "Chapsas", "nick.chapsas@gmail.com", new DateTime(1993, 1, 1), 4);
-            Console.WriteLine("Adding Nick Chapsas was " + (addResult ? "successful" : "unsuccessful"));
+            var addResult = userService.AddUser("Bruno", "Camba", "bruno.camba@gmail.com", new DateTime(1993, 1, 1), 4);
+            Console.WriteLine("Adding Bruno Camba was " + (addResult ? "successful" : "unsuccessful"));
         }
     }
 }
